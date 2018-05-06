011ff4615c80cd7d45ec712675979e0ba1a46dfe
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-05-06 18:27:19 +0500

    add integral curves

--- src/FP/Intro.hs
+++ src/FP/Intro.hs
@@ -1,4 +1,4 @@
-module FP.Intro (exponential) where
+module FP.Intro (exponential, areaUnderCurves) where
 
 truncate' :: Double -> Int -> Double
 truncate' x n = (fromIntegral $ round $ x * t) / t
@@ -8,3 +8,22 @@ exponential :: Double -> Double
 exponential 0.00 = 1.00
 exponential n =
     truncate' (foldr (\x acc -> acc + (n ** x) / (product [1..x])) 1.00 [1..9]) 4
+
+dX = 0.001
+
+getPointers :: Int -> Int -> [Double]
+getPointers l r =
+    takeWhile (<= fromIntegral r) $ iterate (+ dX) (fromIntegral l + dX)
+
+circArea :: Double -> Double
+circArea r = r ^ 2 * pi
+
+evalP :: [Int] -> [Int] -> Double -> Double
+evalP a b x = sum $ zipWith (\a b -> (fromIntegral a) * x ** (fromIntegral b)) a b
+
+areaUnderCurves :: Int -> Int -> [Int] -> [Int] -> [Double]
+areaUnderCurves l r a b = map (\x -> truncate' x 1) [area, volume]
+    where
+        area = sum $ map((* dX) . evalP a b) $ getPointers l r
+        volume = sum $ map((* dX) . circArea . evalP a b) $ getPointers l r
+
--- test/FP/IntroSpec.hs
+++ test/FP/IntroSpec.hs
@@ -1,10 +1,10 @@
 module FP.IntroSpec (introSpec) where
 
 import Test.Hspec (SpecWith, describe, context, it, shouldBe)
-import FP.Intro (exponential)
+import FP.Intro (exponential, areaUnderCurves)
 
 introSpec :: SpecWith ()
-introSpec = describe "Intro" $
+introSpec = describe "Intro" $ do
     context "exponential" $ do
         it "works with 0.00" $ exponential 0.00 `shouldBe` 1.0000
         it "works with 1.00" $ exponential 1.00 `shouldBe` 2.7183
@@ -12,3 +12,6 @@ introSpec = describe "Intro" $
         it "works with 0.5000" $ exponential 0.5000 `shouldBe` 1.6487
         it "works with 5.0000" $ exponential 5.0000 `shouldBe` 143.6895
         it "works with 20.0000" $ exponential 20.0000 `shouldBe` 2423600.1887
+
+    context "areaUnderCurves" $
+        it "works" $ areaUnderCurves 1 4 [1..5] [6..10] `shouldBe` [2435300.3, 26172951168898.6]
