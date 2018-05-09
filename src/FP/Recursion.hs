7468811be3ef1eec346df50c3815e09dc863c84d
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-05-10 00:13:55 +0500

    add fibonacci

--- src/FP/Recursion.hs
+++ src/FP/Recursion.hs
@@ -1,6 +1,14 @@
-module FP.Recursion (gcd') where
+module FP.Recursion (gcd', fib) where
 
 gcd' :: Integral a => a -> a -> a
 gcd' 0 x = x
 gcd' x 0 = x
 gcd' x y = if (rem x y) == 0 then y else gcd' y (rem x y)
+
+----------
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = last $ take n $ fibStream
+    where fibStream = 0 : 1 : zipWith (+) fibStream (tail fibStream)
--- test/FP/RecursionSpec.hs
+++ test/FP/RecursionSpec.hs
@@ -1,11 +1,14 @@
 module FP.RecursionSpec (recursionSpec) where
 
 import Test.Hspec (SpecWith, describe, context, it, shouldBe)
-import FP.Recursion (gcd')
+import FP.Recursion (gcd', fib)
 
 recursionSpec :: SpecWith ()
-recursionSpec = describe "Recursion" $
+recursionSpec = describe "Recursion" $ do
     context "GCD" $ do
         it "first case" $ gcd' 1 5 `shouldBe` 1
         it "second case" $ gcd' 10 100 `shouldBe` 10
         it "third case" $ gcd' 22 131 `shouldBe` 1
+    context "fibonacci" $ do
+        it "first case" $ fib 3 `shouldBe` 1
+        it "second case" $ fib 5 `shouldBe` 3
